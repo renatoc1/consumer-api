6a2fa8304e6fdb4741a177e42bf68d5864379709
Renato Prata <renprata@hotmail.com>
2020-09-03 19:20:02 -0300

    alteração date format

--- src/main/java/com/api/consumer/model/to/CampanhaSocioTorcedorTO.java
+++ src/main/java/com/api/consumer/model/to/CampanhaSocioTorcedorTO.java
@@ -2,10 +2,7 @@ package com.api.consumer.model.to;
 
 import java.time.LocalDate;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,12 +16,10 @@ import lombok.NoArgsConstructor;
 public class CampanhaSocioTorcedorTO {
 
 	@NotNull
-	@Email
 	private String email;
 
 	private String nomeCompleto;
 
-	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dtNascimento;
 
 	private Long idTimeCoracao;
